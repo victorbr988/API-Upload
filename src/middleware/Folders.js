2836a720c2bd11dfb26d38e8f01d9b4c9910d294
victorbr988 <gabrielvghs666@gmail.com>
2023-06-25 19:48:18 -0300

    refactor: verify path after creation folder

--- src/middleware/Folders.js
+++ src/middleware/Folders.js
@@ -5,8 +5,10 @@ const crypto  = require("crypto")
 function createFolderToGroupFiles(req, res, next) {
   const folderId = crypto.randomBytes(8).toString("hex")
   const pathComplete = path.resolve(__dirname, '..', 'tmp', folderId)
-  fs.mkdirSync(pathComplete)
 
+  if(!fs.existsSync(pathComplete)){
+    fs.mkdirSync(pathComplete)
+  }
   req.folderId = folderId
   next()
 }
